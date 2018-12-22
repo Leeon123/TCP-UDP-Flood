486910eacfad54e9fa8678ce13787fc0f9383829
Leeon123 <leeon123ex@gmail.com>
2018-12-22 11:26:53 +0800

    Update flood.py

--- flood.py
+++ flood.py
@@ -22,7 +22,7 @@ def run():
 				s.sendto(data,addr)
 			print(i +" Sent!!!")
 		except:
-			print(i +" Sent!!!")
+			print("[!] Error!!!")
 
 def run2():
 	data = random._urandom(16)
@@ -36,10 +36,8 @@ def run2():
 				s.send(data)
 			print(i +" Sent!!!")
 		except:
-			print(i +" Sent!!!")
-			pass
-			#s.close()
-			#print("[*] Error")
+			s.close()
+			print("[*] Error")
 
 for y in range(threads):
 	if choice == 'y':
